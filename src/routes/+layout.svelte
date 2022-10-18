c2e34a90210ad7969f97ed256d9548195a19c432
CoopTRUE <coopdog4545@gmail.com>
2022-10-17 17:37:43 -0700

    forgot essential code for svelte toast

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,11 @@
 <script>
+  import { SvelteToast } from '@zerodevx/svelte-toast'
   import '../global.scss'
+
+  import { browser } from '$app/environment'
+  import { connect } from '$lib/socket'
+  if (browser) connect()
 </script>
 
 <slot />
+<SvelteToast />
