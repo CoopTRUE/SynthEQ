72865d5e7ed0468fec85e0b6f34292b9be62c780
CoopTRUE <coopdog4545@gmail.com>
2022-10-27 13:22:15 -0700

    precompress off for cloudflare optimizations

--- svelte.config.js
+++ svelte.config.js
@@ -16,7 +16,8 @@ const config = {
   ],
 
   kit: {
-    adapter: adapter({ precompress: true }),
+    adapter: adapter(),
+    // adapter: adapter({ precompress: true }),
     alias: {
       $components: path.resolve(__dirname, './src/lib/components'),
       $client: path.resolve(__dirname, './src/lib/client'),
