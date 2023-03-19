c792e0fa56bff86c70f3a756a6e1558ba17fe9d1
coopTRUE <coopdog4545@gmail.com>
2023-03-19 10:56:18 -0700

    completed create contract api

--- src/routes/api/contract/create/+server.ts
+++ src/routes/api/contract/create/+server.ts
@@ -8,12 +8,12 @@ import NETWORKS from '@constants/networks'
 import * as log from '$lib/logging'
 import prisma from '$lib/server/prisma'
 import redis from '$lib/server/redis'
+import networks from '$lib/server/networks'
 
 const schema = z.object({
   type: z.enum(['buyer', 'shorter']),
   address: z.string().regex(REGEX.address),
   ticker: z.string().refine((ticker) => SERVER.supportedTickers.includes(ticker)),
-  value: z.number().min(POSITION.minValue).max(POSITION.maxValue).int(),
   expiration: z
     .date()
     .min(new Date(Date.now() + POSITION.minExpiration))
@@ -38,11 +38,7 @@ export async function POST({ request }) {
 
   const { address, txnHash, chainId } = parsed.data
 
-  const user = await prisma.user.findUnique({
-    where: {
-      address
-    }
-  })
+  const user = await prisma.user.findUnique({ where: { address } })
   if (!user) {
     log.reqError(`${ip}: User not found`)
     return new Response('User not found', { status: 404 })
@@ -53,15 +49,61 @@ export async function POST({ request }) {
     return new Response('Contract creation already in progress', { status: 409 })
   }
   await redis.sadd('processingContractCreation', txnHash)
-  async function removeProcessingContractCreation() {
+  async function cleanup() {
     await redis.srem('processingContractCreation', txnHash)
   }
 
-  const network = NETWORKS[chainId]
+  const network = networks[chainId]
+  const transaction = await network.getTransaction(txnHash)
+  if (!transaction) {
+    log.reqError(`${ip}: Transaction not found`)
+    await cleanup()
+    return new Response('Transaction not found', { status: 404 })
+  }
+  const receipt = await network.getTransactionReceipt(txnHash)
+  if (!receipt) {
+    log.reqError(`${ip}: Transaction not confirmed`)
+    await cleanup()
+    return new Response('Transaction not confirmed', { status: 400 })
+  }
+  if (!receipt.status) {
+    log.reqError(`${ip}: Transaction failed`)
+    await cleanup()
+    return new Response('Transaction failed', { status: 400 })
+  }
+  if (transaction.from.toLowerCase() !== user.address) {
+    log.reqError(`${ip}: Transaction not from user`)
+    await cleanup()
+    return new Response('Transaction not from user', { status: 400 })
+  }
+  const token = network.tokens.find((token) => token.address === transaction.to)
+  if (!token) {
+    log.reqError(`${ip}: Transaction not to supported token`)
+    await cleanup()
+    return new Response('Transaction not to supported token', { status: 400 })
+  }
+  const args = token.getTransferArgs(transaction.data)
+  if (!args) {
+    log.reqError(`${ip}: Transaction not a transfer`)
+    await cleanup()
+    return new Response('Transaction not a transfer', { status: 400 })
+  }
+  if (args.to !== SERVER.address) {
+    log.reqError(`${ip}: Transaction not to server`)
+    await cleanup()
+    return new Response('Transaction not to server', { status: 400 })
+  }
+  const price = await token.getPrice()
+  const value = args.amount * price
+  if (value < POSITION.minValue || value > POSITION.maxValue) {
+    log.reqError(`${ip}: Transaction value out of range`)
+    await cleanup()
+    return new Response('Transaction value out of range', { status: 400 })
+  }
 
   const createPosition = parsed.data.type === 'buyer' ? createBuyerPosition : createShorterPosition
-  const position = await createPosition(parsed.data)
-  removeProcessingContractCreation()
+  const position = await createPosition({ ...parsed.data, value })
+  await cleanup()
 
   log.createPosition(`${ip}: ${JSON.stringify(position)}`)
   return json({ position })
